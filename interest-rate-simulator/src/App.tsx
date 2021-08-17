26ca5489ea7226b2d8f905d895699f7a670d2312
yusuke-yy <yusuke-yamada.10004@ciexion.com>
2021-08-18 08:57:55 +0900

    全角数字で入力されても半角数字に変換して計算結果を出せるようにした #27

--- interest-rate-simulator/src/App.tsx
+++ interest-rate-simulator/src/App.tsx
@@ -3,21 +3,45 @@ import "./styles.css";
 
 function App() {
   const [inputBorrowing, setInputBorrowing] = useState<number>();
-  const [inputPaybackYears, setInputPaybackYears] = useState<number>();
-  const [inputPaybackMounths, setInputPaybackMounths] = useState<number>();
-  const [inputInterestRate, setInputInterestRate] = useState<number>();
+  let [inputPaybackYears, setInputPaybackYears] = useState<any>();
+  let [inputPaybackMounths, setInputPaybackMounths] = useState<any>();
+  let [inputInterestRate, setInputInterestRate] = useState<any>();
   const onChangeInputBorrowing = (e: any) => setInputBorrowing(e.target.value);
   const onChangeInputPaybackYears = (e: any) => setInputPaybackYears(e.target.value);
   const onChangeInputPaybackMounths = (e: any) => setInputPaybackMounths(e.target.value);
   const onChangeInputInterestRate = (e: any) => setInputInterestRate(e.target.value);
-  const firstinputPaybackYears = inputPaybackYears || 0;
-  const firstinputPaybackMounths = inputPaybackMounths || 0;
+  const zentohaninputBorrowing: any = (inputBorrowing: any) => {
+    return String(inputBorrowing).replace(/[０-９]/g, function (s: any) {
+    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    })
+  }
+  const zentohaninputPaybackYears: any = (inputPaybackYears: any) => {
+    return String(inputPaybackYears).replace(/[０-９]/g, function (s: any) {
+    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    })
+  }
+  const zentohaninputPaybackMounths: any = (inputPaybackMounths: any) => {
+    return String(inputPaybackMounths).replace(/[０-９]/g, function (s: any) {
+    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    })
+  }
+  const zentohaninputInterestRate: any = (inputInterestRate: any) => {
+    return String(inputInterestRate).replace(/[０-９]/g, function (s: any) {
+    return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
+    })
+  }
+  const hankakuinputBorrowing = zentohaninputBorrowing(inputBorrowing)
+  const hankakuinputPaybackYears = zentohaninputPaybackYears(inputPaybackYears)
+  const hankakuinputPaybackMounths = zentohaninputPaybackMounths(inputPaybackMounths)
+  const hankakuinputInterestRate = zentohaninputInterestRate(inputInterestRate)
+  const firstinputPaybackYears = hankakuinputPaybackYears || 0;
+  const firstinputPaybackMounths = hankakuinputPaybackMounths || 0;
   let numPayBack: number = Number(firstinputPaybackYears)*12+Number(firstinputPaybackMounths);
-  const percentages: number = Number(inputInterestRate)/100
+  const percentages: number = Number(hankakuinputInterestRate)/100
   const exponentiation: number = Math.pow((1+percentages), Number(numPayBack));
-  const mounthsPayback: number = Number(inputBorrowing)*(percentages)*exponentiation/(exponentiation-1);
+  const mounthsPayback: number = Number(hankakuinputBorrowing)*(percentages)*exponentiation/(exponentiation-1);
   const numTotalPayBack: number = Number(mounthsPayback) * numPayBack;
-  const totalInterest: number = Number(numTotalPayBack)-Number(inputBorrowing);
+  const totalInterest: number = Number(numTotalPayBack)-Number(hankakuinputBorrowing);
   const nanTotalPayBack = (numTotalPayBack: any) => {
     if (isNaN(numTotalPayBack)) {
       return "-"
@@ -49,14 +73,14 @@ function App() {
         <div className="input-area">
           <div className="borrowing-area">
             <div className="borrowing">借入金額</div>
-            <input className="borrowing-input" type="number" value={inputBorrowing} onChange={onChangeInputBorrowing} />
+            <input className="borrowing-input" value={hankakuinputBorrowing} onChange={onChangeInputBorrowing} />
             <div className="yen">円</div>
           </div>
           <div className="payback-area">
             <div className="payback">返済期間</div>
             <div className="inline-block">
-              <input className="payback_years-input" type="number" value={inputPaybackYears} onChange={onChangeInputPaybackYears}/>
-              <input className="payback_months-input" type="number" value={inputPaybackMounths} onChange={onChangeInputPaybackMounths}/>
+              <input className="payback_years-input" value={hankakuinputPaybackYears} onChange={onChangeInputPaybackYears}/>
+              <input className="payback_months-input" value={hankakuinputPaybackMounths} onChange={onChangeInputPaybackMounths}/>
             </div>
             <div className="inline-block">
               <span className="years">年</span>
@@ -65,7 +89,7 @@ function App() {
           </div>
           <div className="interest-rate-area">
             <div className="interest-rate">金利</div>
-            <input className="interest-rate-input" type="number" value={inputInterestRate} onChange={onChangeInputInterestRate}/>
+            <input className="interest-rate-input" value={hankakuinputInterestRate} onChange={onChangeInputInterestRate}/>
             <div className="percentage">％</div>
           </div>
         </div>
